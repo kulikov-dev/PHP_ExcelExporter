e9c9297bd0f525c39212dc460f9f9bdc13fa4ca2
genki <reward.hunters@gmail.com>
2019-03-06 23:40:40 +0500

    Converter from XLSX-XLS to CSV.

--- ConverterService.php
+++ ConverterService.php
@@ -1,107 +1,140 @@
 <?php
 namespace kulikovdev;
-use kulikovdev\ConverterService as ConverterService;
-require_once 'lib/xlsxwriter.php';
 include 'Autoloader.php';
 
+use kulikovdev\ConverterService as ConverterService;
+use kulikovdev\ExporterService as ExporterService;
+
 class ConverterService {
-	/**
- 	* Config: path from site root folder to folder for saving exported files
- 	*/ 
-	const settingsUrlExportPath = 'TempExport';
-	/**
- 	* Config: relative path from service folder to folder for saving exported files
- 	*/ 
-	const settingsExportPath = "../TempExport/";
-	
-	/**
-	* Copy csv file to another place;
-	* $inputFilePath - relative path to csv file;
-	*/ 
-	private static function CopyCsv($inputFilePath) {
-		$fileName = uniqid() . ".csv";
-		$outputFilePath = ConverterService::settingsExportPath . $fileName;
-		copy($inputFilePath, $outputFilePath);
-				
-		$context = stream_context_create();
-  		$orig_file = fopen($outputFilePath, 'r', 1, $context);
+    /**
+     * Config: path from site root folder to folder for saving exported files
+     */
+    const settingsUrlExportPath = 'TempExport';
+    /**
+     * Config: relative path from service folder to folder for saving exported files
+     */
+    const settingsExportPath = "../TempExport/";
 
-  		$temp_filename = tempnam(sys_get_temp_dir(), 'php_prepend_');
-  		file_put_contents($temp_filename, chr(0xEF).chr(0xBB).chr(0xBF));		// for unicode supporting
-  		file_put_contents($temp_filename, $orig_file, FILE_APPEND);
+    /**
+     * Copy csv file to another place;
+     * $inputFilePath - relative path to csv file;
+     */
+    private static function CopyCsv($inputFilePath) {
+        $fileName = uniqid() . ".csv";
+        $outputFilePath = ConverterService::settingsExportPath . $fileName;
+        copy($inputFilePath, $outputFilePath);
 
- 		fclose($orig_file);
-  		unlink($outputFilePath);
-  		rename($temp_filename, $outputFilePath);
-		return $fileName;
-	}
-	/**
-	* Convert csv file to XLSX format;
-	* $inputFilePath - relative path to csv file;
-	*/ 
-	private static function ConvertToXlsx($inputFilePath) {
-		$handle = fopen($inputFilePath, "r");	
-		$fileName = uniqid() . ".xlsx";
-		$filePath = ConverterService::settingsExportPath . $fileName;
-		$writer = new \XLSXWriter();	
-		
-		while ( ($data = fgetcsv($handle,0,';') ) !== FALSE ) {
-			$writer->writeSheetRow('data', $data);
-		}
-		$writer->writeToFile($filePath);
-		fclose($handle);
-		return $fileName;
-	}
-	/**
-	* Convert csv file to XLS format;
-	* $inputFilePath - relative path to csv file;
-	*/ 
-	private static function ConvertToXls($inputFilePath) {
-		$handle = fopen($inputFilePath, "r");		
-		$fileName = uniqid() . ".xls";
-		$filePath = ConverterService::settingsExportPath . $fileName;
-		$workbook = &new \writeexcel_workbookbig($filePath);
-		$worksheet = &$workbook->addworksheet();
-		$lineCount = 0;
-		while ( ($data = fgetcsv($handle,0,';') ) !== FALSE ) {
-			$array = array_values($data);
-			$subLength = count($array);
-			for ($j = 0; $j < $subLength; $j++) {
-				$worksheet->write($lineCount,$j, (string)$array[$j]);
-			}
-			++$lineCount;
-		}
-	
-		$workbook->close();
-		fclose($handle);
-		return $fileName;
-	}
-	
-	/**
-	 * Convert csv file to Excel file;
-	 * $inputFilePath - relative path to csv file;
-	 * $exportType - output file format
-	 */ 
-	public static function ConvertCsvToExcel($inputFilePath, $exportType) {
-		if (!file_exists($inputFilePath)) {
-			echo 'File not found!';
-			return;
-		}
-		
-		ini_set('auto_detect_line_endings',TRUE);
-		switch ($exportType) {
-    		case ExportTypeEnum.XLSX:
-				$fileName = ConverterService::ConvertToXlsx($inputFilePath);	
-			break;
-			case ExportTypeEnum.XLS:
-				$fileName = ConverterService::ConvertToXls($inputFilePath);
-			break;
-			case ExportTypeEnum.CSV:
-				$fileName = ConverterService::CopyCsv($inputFilePath);
-        	break;
-		}	
-		
-		ini_set('auto_detect_line_endings',FALSE);
-		echo $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER[HTTP_HOST] . "/" . ConverterService::settingsUrlExportPath . "/" . $fileName;
-	}
+        $context = stream_context_create();
+        $orig_file = fopen($outputFilePath, 'r', 1, $context);
+        $temp_filename = tempnam(sys_get_temp_dir(), 'php_prepend_');
+        file_put_contents($temp_filename, chr(0xEF).chr(0xBB).chr(0xBF));		// for unicode supporting
+        file_put_contents($temp_filename, $orig_file, FILE_APPEND);
+        fclose($orig_file);
+        unlink($outputFilePath);
+        rename($temp_filename, $outputFilePath);
+        return $fileName;
+    }
+    /**
+     * Convert csv file to XLSX format;
+     * $inputFilePath - relative path to csv file;
+     */
+    private static function ConvertToXlsx($inputFilePath) {
+        $handle = fopen($inputFilePath, "r");
+        $fileName = uniqid() . ".xlsx";
+        $filePath = ConverterService::settingsExportPath . $fileName;
+        $writer = new \XLSXWriter();
+
+        while ( ($data = fgetcsv($handle,0,';') ) !== FALSE ) {
+            $writer->writeSheetRow('data', $data);
+        }
+        $writer->writeToFile($filePath);
+        fclose($handle);
+        return $fileName;
+    }
+    /**
+     * Convert csv file to XLS format;
+     * $inputFilePath - relative path to csv file;
+     */
+    private static function ConvertToXls($inputFilePath) {
+        $handle = fopen($inputFilePath, "r");
+        $fileName = uniqid() . ".xls";
+        $filePath = ConverterService::settingsExportPath . $fileName;
+        $workbook = &new \writeexcel_workbookbig($filePath);
+        $worksheet = &$workbook->addworksheet();
+        $lineCount = 0;
+        while ( ($data = fgetcsv($handle,0,';') ) !== FALSE ) {
+            $array = array_values($data);
+            $subLength = count($array);
+            for ($j = 0; $j < $subLength; $j++) {
+                $worksheet->write($lineCount,$j, (string)$array[$j]);
+            }
+            ++$lineCount;
+        }
+
+        $workbook->close();
+        fclose($handle);
+        return $fileName;
+    }
+
+    /**
+     * Convert csv file to Excel file;
+     * $inputFilePath - relative path to csv file;
+     * $exportType - output file format
+     */
+    public static function ConvertCsvToExcel($inputFilePath, $exportType) {
+        if (!file_exists($inputFilePath)) {
+            echo 'File not found!';
+            return;
+        }
+
+        ini_set('auto_detect_line_endings',TRUE);
+        switch ($exportType) {
+            case ExportTypeEnum.XLSX:
+                $fileName = ConverterService::ConvertToXlsx($inputFilePath);
+                break;
+            case ExportTypeEnum.XLS:
+                $fileName = ConverterService::ConvertToXls($inputFilePath);
+                break;
+            case ExportTypeEnum.CSV:
+                $fileName = ConverterService::CopyCsv($inputFilePath);
+                break;
+        }
+
+        ini_set('auto_detect_line_endings',FALSE);
+        echo $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER[HTTP_HOST] . "/" . ConverterService::settingsUrlExportPath . "/" . $fileName;
+    }
+
+    /**
+     * Convert xls and xlsx files to csv file;
+     * $inputFilePath - relative path to Excel file;
+     */
+    public static function ConvertExcelToCsv($inputFilePath) {
+        if (!file_exists($inputFilePath)) {
+            echo 'File not found!';
+            return;
+        }
+        $hasError = false;
+        $ext = pathinfo($inputFilePath, PATHINFO_EXTENSION);
+        switch ($ext) {
+            case 'xls':
+                if ( $xlsx = \SimpleXLS::parse($inputFilePath) ) {
+                    $fileName = ExporterService::ExportToCsv($xlsx->rows());
+                } else {
+                    $hasError = true;
+                    echo SimpleXLSX::parseError();
+                }
+                break;
+            case 'xlsx':
+                if ( $xlsx = \SimpleXLSX::parse($inputFilePath) ) {
+                    $fileName = ExporterService::ExportToCsv($xlsx->rows());
+                } else {
+                    $hasError = true;
+                    echo SimpleXLSX::parseError();
+                }
+                break;
+        }
+        if (!$hasError) {
+            echo $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER[HTTP_HOST] . "/" . ExporterService::settingsUrlExportPath . "/" . $fileName;
+        }
+    }
 }
--- ExporterService.php
+++ ExporterService.php
@@ -1,86 +1,85 @@
 <?php
 namespace kulikovdev;
 use kulikovdev\ExporterService as ExporterService;
-require_once 'lib/xlsxwriter.php';
 include 'Autoloader.php';
 
 class ExporterService {
 
-	const settingsUrlExportPath = 'TempExport';
-	/**
- 	* Config: path to save exported files.
- 	*/ 
-	const settingsExportPath = "../TempExport/";
+    const settingsUrlExportPath = 'TempExport';
+    /**
+     * Config: path to save exported files.
+     */
+    const settingsExportPath = "../TempExport/";
 
-	/**
-	 * Export php-array to csv file;
-	 */ 
-	private static function ConvertToCsv($someArray) {
-		$fileName = uniqid() . ".csv";
-		$filePath = ExporterService::settingsExportPath . $fileName;
-   		$delimiter = ';';
-		$temp_memory = fopen($filePath, 'w');
-	    fprintf($temp_memory, chr(0xEF).chr(0xBB).chr(0xBF));
-		foreach ($someArray as $line) {
-			fputcsv($temp_memory, $line, $delimiter);
-		}
+    /**
+     * Export php-array to csv file;
+     */
+    public static function ExportToCsv($someArray) {
+        $fileName = uniqid() . ".csv";
+        $filePath = ExporterService::settingsExportPath . $fileName;
+        $delimiter = ';';
+        $temp_memory = fopen($filePath, 'w');
+        fprintf($temp_memory, chr(0xEF).chr(0xBB).chr(0xBF));
+        foreach ($someArray as $line) {
+            fputcsv($temp_memory, $line, $delimiter);
+        }
 
-		fclose($temp_memory);
-		return $fileName;
-	}
-	/**
-	 * Export php-array to xls file;
-	 */ 
-	private static function ConvertToXls($someArray) {	
-		$fileName = uniqid() . ".xls";
-		$filePath = ExporterService::settingsExportPath . $fileName;
-		$workbook = &new \writeexcel_workbookbig($filePath);
-		$worksheet = &$workbook->addworksheet();
+        fclose($temp_memory);
+        return $fileName;
+    }
+    /**
+     * Export php-array to xls file;
+     */
+    private static function ExportToXls($someArray) {
+        $fileName = uniqid() . ".xls";
+        $filePath = ExporterService::settingsExportPath . $fileName;
+        $workbook = &new \writeexcel_workbookbig($filePath);
+        $worksheet = &$workbook->addworksheet();
 
-		$length = count($someArray);
-		for ($i = 0; $i < $length; $i++) {
-			$subLength = count($someArray[$i]);
-			$subArray = $someArray[$i];
-			for ($j = 0; $j < $subLength; $j++) {
-				$array = array_values($subArray);
-				$worksheet->write($i,$j, (string)$array[$j]);
-			}
-		}
-	
-		$workbook->close();
-		return $fileName;
-	}
-	/**
-	 * Export php-array to xlsx file;
-	 */ 
-	private static function ConvertToXlsx($someArray) {
-		$fileName = uniqid() . ".xlsx";
-		$filePath = ExporterService::settingsExportPath . $fileName;
-		$writer = new \XLSXWriter();
-		$writer->writeSheet($someArray);
-		$writer->writeToFile($filePath);
-		return $fileName;
-	}
+        $length = count($someArray);
+        for ($i = 0; $i < $length; $i++) {
+            $subLength = count($someArray[$i]);
+            $subArray = $someArray[$i];
+            for ($j = 0; $j < $subLength; $j++) {
+                $array = array_values($subArray);
+                $worksheet->write($i,$j, (string)$array[$j]);
+            }
+        }
 
-	/**
-	 * Export json string to table file;
-	 * $json - dataTable data in JSON format
-	 * $exportType - output file format
-	 */ 
-	public static function ExportJsonToFile($json, $exportType) {
-		$someArray = json_decode($json, true);
-		switch ($exportType) {
-    		case ExportTypeEnum.XLSX:
-				$fileName = ExporterService::ConvertToXlsx($someArray);
-			break;
-			case ExportTypeEnum.XLS:
-				$fileName = ExporterService::ConvertToXls($someArray);
-			break;
-			case ExportTypeEnum.CSV:
-        		$fileName = ExporterService::ConvertToCsv($someArray);
-        	break;
-		}	
-		echo $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER[HTTP_HOST] . "/" . ExporterService::settingsUrlExportPath . "/" . $fileName;
-	}
-}	
+        $workbook->close();
+        return $fileName;
+    }
+    /**
+     * Export php-array to xlsx file;
+     */
+    private static function ExportToXlsx($someArray) {
+        $fileName = uniqid() . ".xlsx";
+        $filePath = ExporterService::settingsExportPath . $fileName;
+        $writer = new \XLSXWriter();
+        $writer->writeSheet($someArray);
+        $writer->writeToFile($filePath);
+        return $fileName;
+    }
+
+    /**
+     * Export json string to table file;
+     * $json - dataTable data in JSON format
+     * $exportType - output file format
+     */
+    public static function ExportJsonToFile($json, $exportType) {
+        $someArray = json_decode($json, true);
+        switch ($exportType) {
+            case ExportTypeEnum.XLSX:
+                $fileName = ExporterService::ExportToXlsx($someArray);
+                break;
+            case ExportTypeEnum.XLS:
+                $fileName = ExporterService::ExportToXls($someArray);
+                break;
+            case ExportTypeEnum.CSV:
+                $fileName = ExporterService::ExportToCsv($someArray);
+                break;
+        }
+        echo $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER[HTTP_HOST] . "/" . ExporterService::settingsUrlExportPath . "/" . $fileName;
+    }
+}
 ?>
